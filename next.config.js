4791c559fa0d6988705de1287fb8b2d981fadb09
harshit13-tech <harshitmahi1286@gmail.com>
2026-05-04 13:50:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+    return config;
+  },
+};
 module.exports = nextConfig;
